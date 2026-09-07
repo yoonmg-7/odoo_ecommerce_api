04220034ffcbeea9df91bb7f8fc0f67aa0815962
yoonmg-7 <yoonnandarmg024@gmail.com>
2026-09-07 11:31:24 +0630

    [FIX] website_sale_api: return images to show correctly

--- website_sale_api/models/product_banner.py
+++ website_sale_api/models/product_banner.py
@@ -12,3 +12,15 @@ class ProductBanner(models.Model):
     _inherit = ["image.mixin"]
 
     description = fields.Text(string="Description")
+
+    def _can_return_content(self, field_name=None, access_token=None):
+        """Field to allow to read without login"""
+        if field_name in [
+            "image_1024",
+            "image_1920",
+            "image_256",
+            "image_512",
+            "image_128",
+        ]:
+            return True
+        return super()._can_return_content(field_name, access_token)
